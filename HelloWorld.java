bbbd9cc455f8ca07e08c33a5ebde81e7d3aafad5
hoikincheng <32083683+hoikincheng@users.noreply.github.com>
2020-02-24 17:41:47 -0800

    Add Initial Files for GUI

---
+++ HelloWorld.java
@@ -0,0 +1,45 @@
+ import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.*;
+import javafx.scene.image.Image;
+import javafx.scene.*;
+
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+BackgroundImage myBI= new BackgroundImage(new Image("background_image.png",300,250,false,true),
+            BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+              BackgroundSize.DEFAULT);
+    //then you set to your node
+ root.setBackground(new Background(myBI));
+ 
+ Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
