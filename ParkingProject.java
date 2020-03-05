1873859af71dcb381977ea7550e6f438b28fc8ac
Daria <dariasykuleva@gmail.com>
2020-03-04 23:35:40 -0800

    created insert methods for vehicle, ParkingSpot, and ParkingHistory. When you run the program, it gives an error, because of foreigh key constraint. This is because I haven't created all the inserts yet.

--- ParkingProject.java
+++ ParkingProject.java
@@ -11,26 +11,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.Date;
 
 public class ParkingProject
 {
     public static void main (String[] args) throws Exception {
-        
-    createNewTable();
+
+    java.util.Date sTime = new java.util.Date();
+    java.util.Date eTime = new java.util.Date();
+
+        //createNewTable();
     ParkingProject app = new ParkingProject();
-    app.insert("Raw Materials", 3000);
-    app.insert("Semifinished Goods", 4000);
-    app.insert("Finished Goods", 5000);
-    
-    app.selectAll();
+
+    //the numbers here are temporary (just to test)
+
+    app.insertVehicle(1, "23543");
+    app.insertVehicle(2, "22343");
+    app.insertVehicle(3, "27843");
+
+    app.insertParkingSpot(34, 3, 25346, 254983, "b87");
+    app.insertParkingSpot(67, 1, 23498, 905427, "a85");
+    app.insertParkingSpot(87, 5, 87652, 984463, "b32");
+
+    app.insertParkingHistory(300, 34, 1, new Date(sTime.getTime()), new Date(eTime.getTime()));
+    app.insertParkingHistory(1, 67, 2, new Date(sTime.getTime()), new Date(eTime.getTime()));
+    app.insertParkingHistory(2, 87, 3, new Date(sTime.getTime()), new Date(eTime.getTime()));
+
+        //app.selectAll();
  }
  
  private Connection connect() {
         // SQLite connection string
-        String url = "jdbc:sqlite:C:\\Users\\ao050\\Dropbox\\Winter 2020\\CSS 475 Database System\\ParkingDB\\test.db";
+        String url = "jdbc:mysql://localhost:3306/Parking_Database";
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url, "root", "UWBParkingSystem");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -76,19 +91,52 @@ public class ParkingProject
         }
     }
     
-    public void insert(String name, double capacity) {
-        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+    public void insertParkingHistory(int id, int parkingSpotID, int vID, Date sTime, Date eTime) {
+        String sql = "INSERT INTO ParkingHistory(ID, ParkingSpotID, VehicleID, startTime, endTime) VALUES(?, ?, ?, ?, ?)";
  
         try (Connection conn = this.connect();
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, name);
-            pstmt.setDouble(2, capacity);
+            pstmt.setInt(1, id);
+            pstmt.setInt(2, parkingSpotID);
+            pstmt.setInt(3, vID);
+            pstmt.setDate(4, sTime);
+            pstmt.setDate(5, eTime);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
+    public void insertVehicle(int id, String rfID) {
+        String sql = "INSERT INTO Vehicle(ID, RFID) VALUES(?, ?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.setString(2, rfID);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void insertParkingSpot(int id, int floorID, int sID, int vehTypeID, String spotNum) {
+        String sql = "INSERT INTO ParkingSpot(ID, FloorID, StatusID, VehicleTypeID, SpotNumber) VALUES(?, ?, ?, ?, ?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.setInt(2, floorID);
+            pstmt.setInt(3, sID);
+            pstmt.setInt(4, vehTypeID);
+            pstmt.setString(5, spotNum);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public void update(int id, String name, double capacity) {
         String sql = "UPDATE warehouses SET name = ? , "
                 + "capacity = ? "
@@ -109,7 +157,7 @@ public class ParkingProject
     }
     
     
-    }
+}
     
  
    
