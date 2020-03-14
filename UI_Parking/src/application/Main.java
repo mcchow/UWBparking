23ba62c271d02cc58fef6f0d745b57c24d6fe923
mcchow <44908668+mcchow@users.noreply.github.com>
2020-03-15 05:28:09 +0800

    Add files via upload

--- UI_Parking/src/application/Main.java
+++ UI_Parking/src/application/Main.java
@@ -1,7 +1,7 @@
 /*
  * Javafx UWN Parking UI
  * Developer: Chow Sheung Him Martin 
- * 
+ * This is the UI for UWB parking
  * 
  * 
  * 
@@ -65,23 +65,68 @@ public class Main extends Application {
 	        });
 
 			Button btn3 = new Button();
-	        btn3.setText("find space");
+	        btn3.setText("number of avalible space");
 	        btn3.setOnAction(new EventHandler<ActionEvent>() {
 	       	 
 	            @Override
 	            public void handle(ActionEvent event) {
-	            	
+	            	root.setCenter(countspace());
 
 	            }
 	        });
 			Button btn4 = new Button();
-	        btn4.setText("find space");
-			Button btn5 = new Button();
-	        btn5.setText("find space");
+	        btn4.setText("find space time");
+	        btn4.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	root.setCenter(countspacetime());
 
+	            }
+	        });
+			Button btn5 = new Button();
+	        btn5.setText("find max parking");
+	        btn5.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	root.setCenter(mostpark());
+	            }
+	        });
+	        
+	        Button btn6 = new Button();
+	        btn6.setText("find max parked floor");
+	        btn6.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	root.setCenter(mostparkinbuilding());
+	            }
+	        });
+	        
+	        Button btn7 = new Button();
+	        btn7.setText("find max parked building");
+	        btn7.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	root.setCenter(mostbuilding());
+	            }
+	        });
+	        
+	        Button btn8 = new Button();
+	        btn8.setText("find common car type");
+	        btn8.setOnAction(new EventHandler<ActionEvent>() {
+		       	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	            	root.setCenter(mostcar());
+	            }
+	        });
+	        
 			VBox vbox = new VBox();
 			vbox.setSpacing(10);
-			vbox.getChildren().addAll(btn1,btn2,btn3,btn4,btn5);
+			vbox.getChildren().addAll(btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8);
 			
 			root.setLeft(vbox);
 			root.setCenter(findplace());
@@ -159,7 +204,7 @@ public class Main extends Application {
 		return new Pane();
 	}
 	
-	//creating page for finding space
+	//creating page for finding space record in the past
 		public Pane findplacebytime() {
 			try {
 		        
@@ -198,6 +243,7 @@ public class Main extends Application {
 		        					"  JOIN ParkingHistory ON (ParkingSpot.id = ParkingHistory.id)\r\n" + 
 		        					"  JOIN Building ON (Building.id = floor.buildingId)\r\n" + 
 		        					"Where StartTime < "+ cb.getPromptText() + " AND EndTime > " + cb.getPromptText() + " AND status.description = 'Available';");
+		        			result.getItems().clear();
 		        			while(rs.next())  {
 		        				result.getItems().add("Building: " + rs.getString(1)+" Floor: "+rs.getInt(2)+"  SpotID: "+rs.getInt(3));  
 		        			}
@@ -219,7 +265,375 @@ public class Main extends Application {
 			return new Pane();
 		}
 	
+	//creating page for finding space
+	
+		public Pane countspace() {
+			try {
+				//create all element
+				Button btn = new Button();
+				Text result = new Text();
+				result.setX(50);
+				result.setY(100);
+				
+		        btn.setText("avalible space");
+		        btn.setLayoutX(130);
+		        btn.setLayoutY(200);
+		        // only 2 location is need
+		        ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList(
+		        	    "garage", "ground")
+		        	);
+		        cb.setLayoutX(50);
+		        cb.setLayoutY(200);
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+		 
+		            @Override
+		            public void handle(ActionEvent event) {
+		            	//the query
+		            	try{  
+		        			Class.forName("com.mysql.cj.jdbc.Driver");
+		        			//the first thing is the host id, plz also put serverTimezone=UTC to set up srever time. 
+		        			//root is the name of the user, password 123456 , 
+		        			Connection con=DriverManager.getConnection(  
+		        			"jdbc:mysql://localhost:3306/UWB_Parking?serverTimezone=UTC","root","123456");  
+		        			//here Parking is database name, root is username and password  
+		        			//System.out.println("test");
+		        			//run query
+		        			Statement stmt=con.createStatement();
+		        			//stmt.executeQuery("Insert Into Status (ID,Description) Value(3,'noonecare');");  
+		        			ResultSet rs=stmt.executeQuery("SELECT Floor.FloorNumber, COUNT(SpotNumber) AS Available\r\n" + 
+		        					"FROM ParkingSpot\r\n" + 
+		        					"  JOIN Floor on (Floor.id = ParkingSpot.floorId)\r\n" + 
+		        					"  JOIN Status ON (Status.id = ParkingSpot.statusId)\r\n" + 
+		        					"  JOIN Building ON (Building.id = floor.buildingId)\r\n" + 
+		        					"Where status.description = 'Available' AND Building.name = \""+ (String)cb.getValue() + "\";\r\n"
+		        					);
+		        			String resulttext = "";
+		        			while(rs.next())  {
+		        				resulttext+="Floor: "+rs.getInt(1)+"  remain space: "+rs.getInt(2) +"\n";  
+		        			}
+		        			result.setText(resulttext);
+		        			con.close();  
+		        			}catch(Exception e){ System.out.println(e);}  
+		            }
+		        });
+		        
+		        Pane root = new Pane(); 
+				root.getChildren().add(btn);  
+				root.getChildren().add(result);  
+				root.getChildren().add(cb);
+				return root;
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return new Pane();
+		}
+		
+		public Pane countspacetime() {
+			try {
+				//create all element
+				Button btn = new Button();
+				Text result = new Text();
+				result.setX(50);
+				result.setY(100);
+				
+		        btn.setText("avalible space");
+		        btn.setLayoutX(400);
+		        btn.setLayoutY(200);
+		        // only 2 location is need
+		        ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList(
+		        	    "garage", "ground")
+		        	);
+		        cb.setLayoutX(50);
+		        cb.setLayoutY(200);
+		        TextField cb2 = new TextField();
+		        cb2.setLayoutX(130);
+		        cb2.setLayoutY(200);
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+		 
+		            @Override
+		            public void handle(ActionEvent event) {
+		            	//the query
+		            	try{  
+		        			Class.forName("com.mysql.cj.jdbc.Driver");
+		        			//the first thing is the host id, plz also put serverTimezone=UTC to set up srever time. 
+		        			//root is the name of the user, password 123456 , 
+		        			Connection con=DriverManager.getConnection(  
+		        			"jdbc:mysql://localhost:3306/UWB_Parking?serverTimezone=UTC","root","123456");  
+		        			//here Parking is database name, root is username and password  
+		        			//System.out.println("test");
+		        			//run query
+		        			Statement stmt=con.createStatement();
+		        			//stmt.executeQuery("Insert Into Status (ID,Description) Value(3,'noonecare');");  
+		        			ResultSet rs=stmt.executeQuery("SELECT Floor.FloorNumber, COUNT(SpotNumber) AS Available\r\n" + 
+		        					"FROM ParkingSpot\r\n" + 
+		        					"  JOIN Floor on (Floor.id = ParkingSpot.floorId)\r\n" + 
+		        					"  JOIN Status ON (Status.id = ParkingSpot.statusId)\r\n" + 
+		        					"  JOIN Building ON (Building.id = floor.buildingId)\r\n" + 
+		        					"Where status.description = 'Available' AND Building.name = \""+ (String)cb.getValue() + "\""+
+		        					"StartTime < "+ cb2.getPromptText() + " AND EndTime > " + cb2.getPromptText() +";\r\n"
+		        					);
+		        			String resulttext = "";
+		        			while(rs.next())  {
+		        				resulttext+="Floor: "+rs.getInt(1)+"  remain space: "+rs.getInt(2) +"\n";  
+		        			}
+		        			result.setText(resulttext);
+		        			con.close();  
+		        			}catch(Exception e){ System.out.println(e);}  
+		            }
+		        });
+		        
+		        Pane root = new Pane(); 
+				root.getChildren().add(btn);  
+				root.getChildren().add(result);  
+				root.getChildren().add(cb);
+				root.getChildren().add(cb2);
+				return root;
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return new Pane();
+		}	
+		
+		public Pane mostpark() {
+			try {
+				//create all element
+				Button btn = new Button();
+				Text result = new Text();
+				result.setX(50);
+				result.setY(100);
+				
+		        btn.setText("max park");
+		        btn.setLayoutX(200);
+		        btn.setLayoutY(200);
+		        // only 2 location is need
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+		 
+		            @Override
+		            public void handle(ActionEvent event) {
+		            	//the query
+		            	try{  
+		        			Class.forName("com.mysql.cj.jdbc.Driver");
+		        			//the first thing is the host id, plz also put serverTimezone=UTC to set up srever time. 
+		        			//root is the name of the user, password 123456 , 
+		        			Connection con=DriverManager.getConnection(  
+		        			"jdbc:mysql://localhost:3306/UWB_Parking?serverTimezone=UTC","root","123456");  
+		        			//here Parking is database name, root is username and password  
+		        			//System.out.println("test");
+		        			//run query
+		        			Statement stmt=con.createStatement();
+		        			//stmt.executeQuery("Insert Into Status (ID,Description) Value(3,'noonecare');");  
+		        			ResultSet rs=stmt.executeQuery("SELECT Building.name, Floor.FloorNumber, SpotNumber, COUNT(*) AS Frequency\r\n" + 
+		        					"FROM ParkingHistory\r\n" + 
+		        					"  JOIN parkingSpot ON (ParkingSpot.ID = parkingHistory.parkingSpotID)\r\n" + 
+		        					"  JOIN Floor on (Floor.id = ParkingSpot.floorId)\r\n" + 
+		        					"  JOIN Building ON (Building.id = Floor.buildingId)\r\n" + 
+		        					"GROUP BY ParkingSpotID\r\n" + 
+		        					"ORDER BY Frequency DESC\r\n" + 
+		        					"LIMIT 1;"
+		        					);
+		        			String resulttext = "";
+		        			while(rs.next())  {
+		        				resulttext+="Building: "+rs.getString(1)+"  Floor: "+rs.getInt(2) +"  Spotnumber: "+rs.getInt(3) +"\n";  
+		        			}
+		        			result.setText(resulttext);
+		        			con.close();  
+		        			}catch(Exception e){ System.out.println(e);}  
+		            }
+		        });
+		        
+		        Pane root = new Pane(); 
+				root.getChildren().add(btn);  
+				root.getChildren().add(result);  
+				return root;
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return new Pane();
+		}
+		
+		public Pane mostparkinbuilding() {
+			try {
+				//create all element
+				Button btn = new Button();
+				Text result = new Text();
+				result.setX(50);
+				result.setY(100);
+		        ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList(
+		        	    "garage", "ground")
+		        	);
+		        cb.setLayoutX(50);
+		        cb.setLayoutY(200);
+		        btn.setText("max floor");
+		        btn.setLayoutX(200);
+		        btn.setLayoutY(200);
+		        // only 2 location is need
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+		 
+		            @Override
+		            public void handle(ActionEvent event) {
+		            	//the query
+		            	try{  
+		        			Class.forName("com.mysql.cj.jdbc.Driver");
+		        			//the first thing is the host id, plz also put serverTimezone=UTC to set up srever time. 
+		        			//root is the name of the user, password 123456 , 
+		        			Connection con=DriverManager.getConnection(  
+		        			"jdbc:mysql://localhost:3306/UWB_Parking?serverTimezone=UTC","root","123456");  
+		        			//here Parking is database name, root is username and password  
+		        			//System.out.println("test");
+		        			//run query
+		        			Statement stmt=con.createStatement();
+		        			//stmt.executeQuery("Insert Into Status (ID,Description) Value(3,'noonecare');");  
+		        			ResultSet rs=stmt.executeQuery("SELECT Building.name, Floor.FloorNumber, COUNT(*) AS Frequency\r\n" + 
+		        					"FROM ParkingHistory\r\n" + 
+		        					"  JOIN parkingSpot ON (ParkingSpot.ID = parkingHistory.parkingSpotID)\r\n" + 
+		        					"  JOIN Floor on (Floor.id = ParkingSpot.floorId)\r\n" + 
+		        					"  JOIN Building ON (Building.id = floor.buildingId)\r\n" + 
+		        					"WHERE Building.name = \""+ cb.getValue() +"\"\r\n" + 
+		        					"GROUP BY Floor.floorNumber\r\n" + 
+		        					"ORDER BY Frequency DESC\r\n" + 
+		        					"LIMIT 1;"
+		        					);
+		        			String resulttext = "";
+		        			while(rs.next())  {
+		        				resulttext+="Building: "+rs.getString(1)+"  Floor: "+rs.getInt(2) +"  frequency: "+rs.getInt(3) +"\n";  
+		        			}
+		        			result.setText(resulttext);
+		        			con.close();  
+		        			}catch(Exception e){ System.out.println(e);}  
+		            }
+		        });
+		        
+		        Pane root = new Pane(); 
+				root.getChildren().add(btn);  
+				root.getChildren().add(result); 
+				root.getChildren().add(cb);
+				return root;
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return new Pane();
+		}
+		
+		public Pane mostbuilding() {
+			try {
+				//create all element
+				Button btn = new Button();
+				Text result = new Text();
+				result.setX(50);
+				result.setY(100);
+				
+		        btn.setText("max building");
+		        btn.setLayoutX(200);
+		        btn.setLayoutY(200);
+		        // only 2 location is need
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+		 
+		            @Override
+		            public void handle(ActionEvent event) {
+		            	//the query
+		            	try{  
+		        			Class.forName("com.mysql.cj.jdbc.Driver");
+		        			//the first thing is the host id, plz also put serverTimezone=UTC to set up srever time. 
+		        			//root is the name of the user, password 123456 , 
+		        			Connection con=DriverManager.getConnection(  
+		        			"jdbc:mysql://localhost:3306/UWB_Parking?serverTimezone=UTC","root","123456");  
+		        			//here Parking is database name, root is username and password  
+		        			//System.out.println("test");
+		        			//run query
+		        			Statement stmt=con.createStatement();
+		        			//stmt.executeQuery("Insert Into Status (ID,Description) Value(3,'noonecare');");  
+		        			ResultSet rs=stmt.executeQuery("SELECT Building.name, COUNT(*) AS Frequency\r\n" + 
+		        					"FROM ParkingHistory\r\n" + 
+		        					"  JOIN parkingSpot ON (ParkingSpot.ID = parkingHistory.parkingSpotID)\r\n" + 
+		        					"  JOIN Floor on (Floor.id = ParkingSpot.floorId)\r\n" + 
+		        					"  JOIN Building ON (Building.id = floor.buildingId)\r\n" + 
+		        					"GROUP BY Building.id\r\n" + 
+		        					"ORDER BY Frequency DESC\r\n" + 
+		        					"LIMIT 1;"
+		        					);
+		        			String resulttext = "";
+		        			while(rs.next())  {
+		        				resulttext+="Building: "+rs.getString(1)+"  Number: "+rs.getInt(2) +"\n";  
+		        			}
+		        			result.setText(resulttext);
+		        			con.close();  
+		        			}catch(Exception e){ System.out.println(e);}  
+		            }
+		        });
+		        
+		        Pane root = new Pane(); 
+				root.getChildren().add(btn);  
+				root.getChildren().add(result);  
+				return root;
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return new Pane();
+		}
+		
+		public Pane mostcar() {
+			try {
+				//create all element
+				Button btn = new Button();
+				Text result = new Text();
+				result.setX(50);
+				result.setY(100);
+				
+		        btn.setText("find common car");
+		        btn.setLayoutX(200);
+		        btn.setLayoutY(200);
+		        // only 2 location is need
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+		 
+		            @Override
+		            public void handle(ActionEvent event) {
+		            	//the query
+		            	try{  
+		        			Class.forName("com.mysql.cj.jdbc.Driver");
+		        			//the first thing is the host id, plz also put serverTimezone=UTC to set up srever time. 
+		        			//root is the name of the user, password 123456 , 
+		        			Connection con=DriverManager.getConnection(  
+		        			"jdbc:mysql://localhost:3306/UWB_Parking?serverTimezone=UTC","root","123456");  
+		        			//here Parking is database name, root is username and password  
+		        			//System.out.println("test");
+		        			//run query
+		        			Statement stmt=con.createStatement();
+		        			//stmt.executeQuery("Insert Into Status (ID,Description) Value(3,'noonecare');");  
+		        			ResultSet rs=stmt.executeQuery("SELECT VehicleType.description AS MostCommonType, COUNT(*) AS Frequency\r\n" + 
+		        					"FROM ParkingHistory\r\n" + 
+		        					"   JOIN ParkingSpot ON (ParkingHistory.parkingSpotId = parkingSpot.Id)\r\n" + 
+		        					"   JOIN VehicleType ON (VehicleType.ID = ParkingSpot.vehicleTypeId)\r\n" + 
+		        					"GROUP BY VehicleType.Id\r\n" + 
+		        					"ORDER BY Frequency;"
+		        					);
+		        			String resulttext = "";
+		        			while(rs.next())  {
+		        				resulttext+="MostCommonType: "+rs.getString(1)+"  Number: "+rs.getInt(2) +"\n";  
+		        			}
+		        			result.setText(resulttext);
+		        			con.close();  
+		        			}catch(Exception e){ System.out.println(e);}  
+		            }
+		        });
+		        
+		        Pane root = new Pane(); 
+				root.getChildren().add(btn);  
+				root.getChildren().add(result);  
+				return root;
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			return new Pane();
+		}
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+
 }
